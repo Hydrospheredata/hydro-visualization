5a4a707eaedd985d505e896faeb9f5a289793f7a
beardedwhale <Lisch.batanina@gmail.com>
2019-12-13 13:32:35 +0300

    api sketch

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # hydro-vis
 Service for visualisation of high dimensional for hydrosphere
+
+
+## API
+
+1. GET /visuzalize/transformer
+    transformer - manifold learning transformer from ["umap", "trimap", "tsne"]. For now only ["umap"].
+    
+    parameters:
+        - model_name
+        - model_version
+        
+2. POST /set_params
+    
+    parameters:
+        - model_name
+        - model_version
+        - json with parameters
+    
+    json format:
+    ```json
+   {
+   "transofmer": "umap",
+   "parameters": {"n_neighbours": 15,
+                  "min_dist": 0.1,
+                  "metric":  "cosine"}
+    }
+    ```
+    
---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/visualize/<method>', methods=['GET'])
+def visualize(method):
+    '''
+
+    :param method: umap/trimap/tsne
+    :return: json with:
+                - data_shape (N, n_dimensions)
+                - data
+                - class_labels
+                    - ground_truth
+                    - predicted
+                    - confidences
+                - anomaly_labels
+                    - anomaly_labels
+                    - anomaly_confidence
+                - top_100
+    '''
+    model_name = request.args.get('model_name')
+    model_version = request.args.get('model_version')
+
+
+
+@app.route('/set_params/', methods=['POST'])
+def set_params():
+    model_name = request.args.get('model_name')
+    model_version =request.args.get('model_version')
+    method_params = request.get_json()
+    return 200
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)

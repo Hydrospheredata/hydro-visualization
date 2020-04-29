535d2a7d7f222e926d26ab77a8b0d2f29e466429
beardedwhale <Lisch.batanina@gmail.com>
2020-04-29 15:48:52 +0300

    Added explicit AWS key setting

--- conf.py
+++ conf.py
@@ -1,4 +1,7 @@
 import os
+
+from loguru import logger as logging
+
 DEBUG_ENV = bool(os.getenv("DEBUG_ENV", True))
 APP_PORT = int(os.getenv("APP_PORT", 5000))
 SERVING_URL = os.getenv("SERVING_URL", "managerui:9090")
@@ -12,4 +15,8 @@ MONGO_PASS = os.getenv("MONGO_PASS")
 AWS_STORAGE_ENDPOINT = os.getenv('AWS_STORAGE_ENDPOINT', '')
 FEATURE_LAKE_BUCKET = os.getenv('FEATURE_LAKE_BUCKET', 'feature-lake')
 HYDRO_VIS_BUCKET_NAME = os.getenv('BUCKET_NAME', 'hydro-vis')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', '')
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', '')
 BATCH_SIZE = 10
+if not AWS_ACCESS_KEY_ID or not AWS_SECRET_ACCESS_KEY:
+    logging.warning('No AWS keys in environment variables')

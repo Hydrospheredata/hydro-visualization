7dc486f6d226b2ab326b978f934ca7c12cdf0bf8
akastav <switch@bk.ru>
2021-04-08 11:46:57 +0300

    fix checkout with branch

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -16,7 +16,11 @@ REGISTRYURL = 'hydrosphere'
 GITHUBREPO  = "github.com/Hydrospheredata/hydro-visualization.git"
 
 def checkoutRepo(String repo){
-  git changelog: false, credentialsId: 'HydroRobot_AccessToken', poll: false, url: repo
+  if (env.CHANGE_ID != null ){
+    git changelog: false, credentialsId: 'HydroRobot_AccessToken', poll: false, url: repo, branch: env.CHANGE_BRANCH
+  } else {
+    git changelog: false, credentialsId: 'HydroRobot_AccessToken', poll: false, url: repo, branch: env.BRANCH_NAME
+  }
 }
 
 def getVersion(){
